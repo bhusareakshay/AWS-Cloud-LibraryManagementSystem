2e92e90c86ebb507adb2e642636132a4dc4d34dc
bhusareakshay <bhusare.a@husky.neu.edu>
2019-06-05 15:59:24 -0700

    Created Home Page Controller

---
+++ webapp/LibrarySystem/src/main/java/com/neu/library/controller/HomePageController.java
@@ -0,0 +1,26 @@
+package com.neu.library.controller;
+
+import java.util.Date;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.neu.library.response.ApiResponse;
+
+@RestController
+public class HomePageController {
+	
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public ResponseEntity<Object> displayDate(){
+		ApiResponse apiResponse;
+		
+		apiResponse = new ApiResponse(HttpStatus.OK, new Date().toString(), "NA");
+		
+		return new ResponseEntity<Object>(apiResponse, new HttpHeaders(),HttpStatus.OK);
+	}
+
+}
