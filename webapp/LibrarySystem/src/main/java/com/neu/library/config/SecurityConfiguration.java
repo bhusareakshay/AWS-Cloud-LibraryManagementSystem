e9b16dbbbb4c214f1e810a809c3f4e0e73aac263
PratikPatil1993 <patil.prat@husky.neu.edu>
2019-06-05 23:02:59 -0700

    implimenting book endpoint for get and delete

--- webapp/LibrarySystem/src/main/java/com/neu/library/config/SecurityConfiguration.java
+++ webapp/LibrarySystem/src/main/java/com/neu/library/config/SecurityConfiguration.java
@@ -1,24 +1,39 @@
 package com.neu.library.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
+import ch.qos.logback.classic.Logger;
+
+@EnableWebSecurity
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Autowired
+	CustomAuthenticationProvider customAuthenticationProvider;
+
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		httpSecurity.httpBasic().and().authorizeRequests().antMatchers("/").authenticated();
 		httpSecurity.csrf().disable();
+		//Logger.info("in101");
+		
 	}
-	@Autowired
-	CustomAuthenticationProvider customAuthenticationProvider;
-
+	public void configure(WebSecurity web) throws Exception 
+	{
+		web.ignoring().antMatchers(HttpMethod.POST, "/user/register/");
+}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-	    auth.authenticationProvider(customAuthenticationProvider);
+	  auth.authenticationProvider(customAuthenticationProvider);
 	}
 }
--- webapp/LibrarySystem/src/main/java/com/neu/library/dao/BookDAO.java
+++ webapp/LibrarySystem/src/main/java/com/neu/library/dao/BookDAO.java
@@ -2,6 +2,7 @@ package com.neu.library.dao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
@@ -13,8 +14,30 @@ public class BookDAO {
 	@PersistenceContext
 	private EntityManager entityManager;
 	
+	@Transactional
 	public Book saveBook(Book book) {
 		this.entityManager.persist(book);
 		return book;
+	}
+	
+	public Book getBookById(String id) {
+		
+		Book book;
+		book = entityManager.find(Book.class,id);
+		return book;
+		
+		
+	}
+	
+	@Transactional
+	public void deleteById(Book book) {
+			entityManager.remove(book);
+	flushAndClear();
+		
+	}
+	
+	void flushAndClear() {
+	    entityManager.flush();
+	    entityManager.clear();
 	}
 }
