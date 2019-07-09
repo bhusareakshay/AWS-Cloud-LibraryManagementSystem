399dc20c29ae38a3d182d9c88fef540aac4495cb
bhusareakshay <43082119+bhusareakshay@users.noreply.github.com>
2019-07-08 23:42:43 -0400

    Update startApplication.sh

--- infrastructure/aws/codedeploy/startApplication.sh
+++ infrastructure/aws/codedeploy/startApplication.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+#! /bin/bash
 
-echo "#! /bin/bash" > /tmp/tempFile.sh
+echo "Test File" > /tmp/tempFile.txt
+echo "Starting tomcat" > /tmp/tempFile.txt
+sudo sh /opt/tomcat/bin/startup.sh
+echo "Starting tomcat complete" > /tmp/tempFile.txt
