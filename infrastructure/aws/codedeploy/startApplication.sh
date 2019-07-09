46f72597d17beb9f609c76832e2f20099e630617
PratikPatil1993 <43221496+PratikPatil1993@users.noreply.github.com>
2019-07-08 20:07:02 -0700

    Update startApplication.sh

--- infrastructure/aws/codedeploy/startApplication.sh
+++ infrastructure/aws/codedeploy/startApplication.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo cd /opt/tomcat
-chmod 777 tomcat
+sudo chmod 777 tomcat
 cd webapps
-chmod 777 webapps
+sudo chmod 777 webapps
 vi test.txt
 sudo ./startup.sh
 echo "start application called"
