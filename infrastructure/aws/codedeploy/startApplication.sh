ae0ed1f76b89686b42b1290ebf3150d26f75e702
bhusareakshay <43082119+bhusareakshay@users.noreply.github.com>
2019-07-10 19:21:43 -0400

    Update startApplication.sh

--- infrastructure/aws/codedeploy/startApplication.sh
+++ infrastructure/aws/codedeploy/startApplication.sh
@@ -2,6 +2,7 @@
 
 cd /opt/tomcat/webapps/
 chown -R centos:centos *
-sudo source /etc/profile
-sudo sh /opt/tomcat/bin/startup.sh 
+cat "source /etc/profile" > /opt/tomcat/bin/startup_new.sh
+cat /opt/tomcat/bin/startup.sh >> /opt/tomcat/bin/startup_new.sh
+sudo sh /opt/tomcat/bin/startup_new.sh 
 echo "Starting tomcat complete 2" > /tmp/tempFile.txt
