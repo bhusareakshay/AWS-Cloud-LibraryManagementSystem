2ce95737f8f00d2fbb691994a0a51fc2f53d783c
bhusareakshay <43082119+bhusareakshay@users.noreply.github.com>
2019-07-23 22:57:59 -0400

    Update startApplication.sh

--- infrastructure/aws/codedeploy/startApplication.sh
+++ infrastructure/aws/codedeploy/startApplication.sh
@@ -5,7 +5,7 @@ sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
     -m ec2 \
     -c file:/opt/cloudwatch-config.json \
     -s
-
+sudo systemctl restart amazon-cloudwatch-agent
 cd /opt/tomcat/webapps/
 chown -R centos:centos *
 echo "source /etc/profile" > /opt/tomcat/bin/startup_new.sh
